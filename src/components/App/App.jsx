9978ba14706487dcf36715c89a03ee23397eb902
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-08 21:53:31 +0100

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+
+import { ContactForm } from '../ContactForm/ContactForm';
+import { Filter } from '../Filter/Filter';
+import { ContactList } from '../ContactList/ContactList';
+import style from './App.module.css';
+
+export const App=()=> {
+
+    return (
+      <div className={style.form}>
+        <h1>Phonebook</h1>
+        <ContactForm  />
+        <h2>Contacts</h2>
+        <Filter  />
+        <ContactList
+      
+        />
+      </div>
+    );
+  }
+
+
+
