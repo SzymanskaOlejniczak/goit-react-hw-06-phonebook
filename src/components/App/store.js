cce29ac6ea890e9c02d3f3478ac87d259f413c46
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-10 12:33:06 +0100

    Delete store.js

--- src/components/App/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { contactsReducer } from "../../redux/slice";
-
-export const store = configureStore({
-    reducer: {
-        contactsReducer,
-    },
-});
