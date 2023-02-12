2884513db4edff58eb61cf54041b74ea0d7e1dd6
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-12 23:38:41 +0100

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,18 @@
-
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { Filter } from '../Filter/Filter';
+import { getContacts } from 'redux/selectors';
 import { ContactList } from '../ContactList/ContactList';
 import style from './App.module.css';
 
 export const App=()=> {
-
+  const contacts = useSelector(getContacts);
+  useEffect(() => {
+    if (contacts !== []) {
+      localStorage.setItem('savedContacts', JSON.stringify(contacts));
+    }
+  }, [contacts]);
     return (
       <div className={style.form}>
         <h1>Phonebook</h1>
